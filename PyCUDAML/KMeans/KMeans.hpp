803d812a72bdaafbc2177e329acad0154234f2c7
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-28 23:14:24 -0700

    added inlude guard

--- PyCUDAML/KMeans/KMeans.hpp
+++ PyCUDAML/KMeans/KMeans.hpp
@@ -1,3 +1,6 @@
+#ifndef __KMEANS_HPP__
+#define __KMEANS_HPP__
+
 void kmeans(int k, const float **X,
             int n, int d,
             int max_iter, float threshold,
@@ -20,3 +23,5 @@ void calc_cluster_centers(int k, const float **X, int n, int d,
                           const int *cluster_assignments, float **cluster_centers);
 
 bool is_terminated(int cur_iter, int max_iter, int delta, int n, float threshold);
+
+#endif /* __KMEANS_HPP__ */
